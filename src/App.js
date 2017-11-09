f130e6ba5ebd1385b444d77c354aa305323615d3
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2017-11-09 17:44:10 -0200

    adding antd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Row, Col } from 'antd';
 
 class App extends Component {
   render() {
@@ -13,6 +14,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Row>
+          <Col xs={2} sm={4} md={6} lg={8} xl={10}>Coluna 1</Col>
+          <Col xs={20} sm={16} md={12} lg={8} xl={4}>Coluna 2</Col>
+          <Col xs={2} sm={4} md={6} lg={8} xl={10}>Coluna 3</Col>
+        </Row>
       </div>
     );
   }
