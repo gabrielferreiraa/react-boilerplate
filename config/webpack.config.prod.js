c457b8894789a95d4f9dd6044ac1570dea2ea19d
Gabriel Ferreira <gabrielferreira.95@hotmail.com>
2017-12-21 19:03:19 -0200

    fix alias on webpack prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -89,10 +89,14 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-
-      // Support React Native Web
-      // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web'
+      'react-native': 'react-native-web',
+      'containers': paths.appContainer,
+      'components': paths.appComponents,
+      'stores': paths.appStores,
+      'reduxFlow': paths.appRedux,
+      'reducers': paths.appReducers,
+      'src': paths.appSrc,
+      'utils': paths.appUtils
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
